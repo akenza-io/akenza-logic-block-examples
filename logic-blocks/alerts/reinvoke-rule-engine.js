686f03828abbaed17f711ec597addf9124ab58f8
David Giger <david@akenza.io>
2023-06-01 12:48:48 +0200

    fix: adds a note about the topic

--- logic-blocks/alerts/reinvoke-rule-engine.js
+++ logic-blocks/alerts/reinvoke-rule-engine.js
@@ -3,15 +3,17 @@
 // usage examples: aggregate/process the result(s) from the current script output in a different rule which is invoked after the current rule
 
 function consume(event) {
-  const temperature = event.inputs["temperature"];
-  const humidity = event.inputs["humidity"];
+  const temperature = event.inputs['temperature'];
+  const humidity = event.inputs['humidity'];
 
   // do something with the data
 
-  emit("action", {
+  emit('action', {
     data: {
       temperature,
       humidity
-    }
+    },
+    // NOTE: the topic defined here will overwrite the one specified in the output-connector. this is optional
+    topic: 'custom-topic'
   });
 }
