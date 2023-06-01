a2f94d8b95ec8f3618c4f5019d2ecd3b826492bf
David Giger <david@akenza.io>
2023-06-01 12:58:08 +0200

    fix: adds a real-world example

--- logic-blocks/alerts/reinvoke-rule-engine.js
+++ logic-blocks/alerts/reinvoke-rule-engine.js
@@ -3,17 +3,30 @@
 // usage examples: aggregate/process the result(s) from the current script output in a different rule which is invoked after the current rule
 
 function consume(event) {
-  const temperature = event.inputs['temperature'];
-  const humidity = event.inputs['humidity'];
+    const temp = event.inputs['temperature'];
+    const humidity = event.inputs['humidity'];
 
-  // do something with the data
+    // do something with the data, i.e. calculate the dew-point which can then be used in another rule to trigger an action
+    const constA = 17.625;
+    const constB = 243.04;
+    // calculate numerator of Td equation
+    const dewNumerator = constB * (Math.log(humidity / 100.0) + ((constA * eval(temp)) / (eval(temp) + constB)));
+    // calculate denominator of Td equation
+    const dewDenominator = constA - Math.log(humidity / 100.0) - ((constA * eval(temp)) / (eval(temp) + constB));
+    // calculate the dew point
+    let dewPoint = dewNumerator / dewDenominator;
+    // convert dew to F
+    dewPoint = (dewPoint * (9 / 5)) + 32;
+    // round dew
+    dewPoint = Math.round(dewPoint);
 
-  emit('action', {
-    data: {
-      temperature,
-      humidity
-    },
-    // NOTE: the topic defined here will overwrite the one specified in the output-connector. this is optional
-    topic: 'custom-topic'
-  });
+    emit('action', {
+        // NOTE: the emitted action is expected to contain the data object which is used to re-invoke the rule-engine again
+        // other properties can be added, but will not be present in the subsequent re-invocation
+        data: {
+            dewPoint
+        },
+        // NOTE: the topic defined here will overwrite the one specified in the output-connector. this is optional
+        topic: 'custom-topic'
+    });
 }
