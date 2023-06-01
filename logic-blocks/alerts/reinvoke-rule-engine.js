839312f783949ac8111fbe4d84585b216cc5af36
David Giger <david@akenza.io>
2023-06-01 12:46:43 +0200

    feat: add an example to re-invoke the rule-engine

---
+++ logic-blocks/alerts/reinvoke-rule-engine.js
@@ -0,0 +1,17 @@
+// An example logic block that emits the action in a format which is ready to re-invoke the rule-engine again
+
+// usage examples: aggregate/process the result(s) from the current script output in a different rule which is invoked after the current rule
+
+function consume(event) {
+  const temperature = event.inputs["temperature"];
+  const humidity = event.inputs["humidity"];
+
+  // do something with the data
+
+  emit("action", {
+    data: {
+      temperature,
+      humidity
+    }
+  });
+}
